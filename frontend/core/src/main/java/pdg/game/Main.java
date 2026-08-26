9e0cfd94473d639bc85bb5c98390a24551c76bfc
NicoletJonathan <jonathannicolet@icloud.com>
2026-08-26 12:53:39 +0200

    fixedd package issues

--- frontend/core/src/main/java/pdg/game/Main.java
+++ frontend/core/src/main/java/pdg/game/Main.java
@@ -1,6 +1,7 @@
 package pdg.game;
 
 import com.badlogic.gdx.Game;
+import pdg.game.screens.FirstScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
