a3ca35e6d3a5a0e5a67280ebd119a1437f16cad3
NicoletJonathan <jonathannicolet@icloud.com>
2026-08-27 15:20:59 +0200

    fixed compatibility issues between java 21 and html

--- frontend/core/src/main/java/pdg/game/screens/FirstScreen.java
+++ frontend/core/src/main/java/pdg/game/screens/FirstScreen.java
@@ -3,12 +3,14 @@ package pdg.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import pdg.game.Main;
@@ -86,6 +88,13 @@ public class FirstScreen implements Screen {
     // ui central avec boutons
     TextButton button1 = new TextButton("cancel", skin, "red");
     TextButton button2 = new TextButton("accept", skin, "green");
+    button2.addListener(
+        new ClickListener() {
+          @Override
+          public void clicked(InputEvent event, float x, float y) {
+            background.change();
+          }
+        });
     button1.setSize(100, 30);
     button2.setSize(100, 30);
     frame = new Frame(400, 300, "Connection", button1, button2);
