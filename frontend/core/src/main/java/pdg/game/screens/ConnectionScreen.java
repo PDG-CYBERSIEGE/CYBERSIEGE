779615f21d49f69da38db719990418aaa7edf97b
NicoletJonathan <jonathannicolet@icloud.com>
2026-09-01 15:02:02 +0200

    spotless apply

--- frontend/core/src/main/java/pdg/game/screens/ConnectionScreen.java
+++ frontend/core/src/main/java/pdg/game/screens/ConnectionScreen.java
@@ -1,7 +1,5 @@
 package pdg.game.screens;
 
-import java.util.function.Consumer;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
@@ -18,7 +16,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.FocusListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-
+import java.util.function.Consumer;
 import pdg.game.Main;
 import pdg.game.network.AuthClient;
 import pdg.game.network.HttpClient;
