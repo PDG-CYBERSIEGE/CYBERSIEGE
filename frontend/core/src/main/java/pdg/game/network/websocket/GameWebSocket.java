da6ff5ccfa20a2652df5d02abccc5dc047d775c8
Aber37 <arnaud.bersier@heig-vd.ch>
2026-09-01 10:06:08 +0200

    Client Websocket tests

--- frontend/core/src/main/java/pdg/game/network/websocket/GameWebSocket.java
+++ frontend/core/src/main/java/pdg/game/network/websocket/GameWebSocket.java
@@ -2,6 +2,8 @@ package pdg.game.network.websocket;
 
 public interface GameWebSocket {
   void connect(String baseUrl, String jwt, GameListener gameListener);
+
   void send(String message);
+
   void disconnect();
 }
